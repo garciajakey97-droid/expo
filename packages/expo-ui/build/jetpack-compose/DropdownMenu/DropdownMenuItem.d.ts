bce2dbf03139776843c42827f787fe1840410a74
Jakub Tkacz <jtkacz@expo.io>
2026-03-09 19:11:26 +0100

    [expo-ui][android] rename ContextMenu to DropdownMenu

---
+++ packages/expo-ui/build/jetpack-compose/DropdownMenu/DropdownMenuItem.d.ts
@@ -0,0 +1,39 @@
+/**
+ * Props of the `DropdownMenuItem` component.
+ */
+export type DropdownMenuItemProps = {
+    /**
+     * The text label displayed in the menu item.
+     */
+    text: string;
+    /**
+     * Whether the menu item is enabled.
+     * @default true
+     */
+    enabled?: boolean;
+    /**
+     * Callback that is called when the menu item is pressed.
+     */
+    onPress?: () => void;
+    /**
+     * Slot children for leading/trailing icons.
+     */
+    children?: React.ReactNode;
+};
+/**
+ * A dropdown menu item component that wraps Compose's `DropdownMenuItem`.
+ * Should be used inside `DropdownMenu.Items`.
+ *
+ * @platform android
+ */
+declare function DropdownMenuItemComponent(props: DropdownMenuItemProps): import("react").JSX.Element;
+declare namespace DropdownMenuItemComponent {
+    var LeadingIcon: (props: {
+        children: React.ReactNode;
+    }) => import("react").JSX.Element;
+    var TrailingIcon: (props: {
+        children: React.ReactNode;
+    }) => import("react").JSX.Element;
+}
+export { DropdownMenuItemComponent as DropdownMenuItem };
+//# sourceMappingURL=DropdownMenuItem.d.ts.map
--- packages/expo-ui/src/jetpack-compose/DropdownMenu/DropdownMenuItem.tsx
+++ packages/expo-ui/src/jetpack-compose/DropdownMenu/DropdownMenuItem.tsx
@@ -63,7 +63,7 @@ function TrailingIcon(props: { children: React.ReactNode }) {
 
 /**
  * A dropdown menu item component that wraps Compose's `DropdownMenuItem`.
- * Should be used inside `ContextMenu.Items`.
+ * Should be used inside `DropdownMenu.Items`.
  *
  * @platform android
  */
